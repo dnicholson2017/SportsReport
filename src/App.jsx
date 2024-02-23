093a7ea08b81f751a61ba861907df3f4c08f2c55
dnicholson2017 <dnicholson2017@fau.edu>
2024-02-23 00:11:31 -0500

    connect youtube api and dynamically render card components with the video information displayed but no video embedded yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import React, { useState } from "react";
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from "react";
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
@@ -11,7 +9,50 @@ import LeftRail from "./components/left-rail";
 import Card from "./components/card";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [playlistId, setPlaylistId] = useState(null);
+  const [list2, setList2] = useState(null);
+
+  
+  useEffect(() => {
+    const callYTAPI = async () => {
+      try {
+        const response = await fetch("https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&forHandle=%40NBA&key=AIzaSyC4TailqItlEi4rCx_51CBNubMmw-tQ0-4");
+        const data = await response.json();
+        const playlist = data.items[0].contentDetails.relatedPlaylists.uploads;
+        console.log(data);
+        console.log(playlist);
+        setPlaylistId(playlist);
+      }
+      catch (error) {
+        console.error("Error fetching data", error);
+        throw error
+      }
+    }
+
+    callYTAPI().catch(console.error);
+  }, [])
+
+  useEffect(() => {
+    const callYTAPI2 = async () => {
+      try {
+        if (playlistId) {
+          const response = await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=10&playlistId=${playlistId}&key=AIzaSyC4TailqItlEi4rCx_51CBNubMmw-tQ0-4`);
+          const data = await response.json();
+          console.log(data);
+          console.log(playlistId);
+          setList2(data);
+        }
+      }
+      catch (error) {
+        console.error("Error fetching data", error);
+        throw error
+      }
+    }
+
+    callYTAPI2().catch(console.error);
+  }, [playlistId])
+
+
 
   return (
     <div>
@@ -19,7 +60,9 @@ function App() {
       <Navbar/>
       <div class="container">
         <LeftRail/>
-        <Card/>
+        <Card
+          list2={list2}
+        />
         <RightRail/>
       </div>
     </div>
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,25 +1,20 @@
 import React from "react";
-let Card = () => {
-    return (
-        <div>
-          <div className="card" style={{width: '18rem'}}>
-              <img src="https://images.pexels.com/photos/46798/the-ball-stadion-football-the-pitch-46798.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="Dummy Icon"/>
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-              </div>
-          </div>
-          <div className="card" style={{width: '18rem'}}>
-              <img src="https://images.pexels.com/photos/46798/the-ball-stadion-football-the-pitch-46798.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="Dummy Icon"/>
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-              </div>
+
+let Card = (props) => {
+  return (
+    <div>
+      {props.list2 && props.list2.items.map((item, index) => (
+        <div className="card" style={{width: '18rem'}} key={index}>
+          <img src={item.snippet.thumbnails.default.url} className="card-img-top" alt="Thumbnail"/>
+          <div className="card-body">
+            <h5 className="card-title">{item.snippet.title}</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
           </div>
         </div>
-    )
+      ))}
+    </div>
+  )
 }
 
 export default Card;
